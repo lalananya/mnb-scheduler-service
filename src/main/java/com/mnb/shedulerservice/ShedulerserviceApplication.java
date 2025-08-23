87ace3433af773b4f846ddb96b1b0479f7e0570d
ananya lal <ananya.lal@airtel.com>
2025-08-23 23:34:33 +0530

    mnb-scheduler-service-dependencies

---
+++ src/main/java/com/mnb/shedulerservice/ShedulerserviceApplication.java
@@ -0,0 +1,13 @@
+package com.mnb.shedulerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShedulerserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShedulerserviceApplication.class, args);
+	}
+
+}
